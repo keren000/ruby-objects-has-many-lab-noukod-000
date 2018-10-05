1a86b17300688d5b25760b45b84426f6ceb10657
Keren-Happuc Fleurimond <keren@noukod.com>
2018-10-05 19:07:54 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,22 +1,22 @@
-class Post 
+class Post
 
-  attr_accessor :title, :author 
-  @@all = [] 
+  attr_accessor :title, :author
+  @@all = []
 
   def initialize(title)
-    @title = title 
-    @@all << self 
-  end 
+    @title = title
+    @@all << self
+  end
 
-  def self.all 
-    @@all 
-  end 
+  def self.all
+    @@all
+  end
 
-  def author_name 
-    if author == nil 
-      nil 
+  def author_name
+    if author == nil
+      nil
     else
-      self.author.name 
-    end 
-  end 
+      self.author.name
+    end
+  end
 end 
