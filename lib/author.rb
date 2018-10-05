c5531ad748a0860c81054b3af9d481e4910beb65
Keren-Happuc Fleurimond <keren@noukod.com>
2018-10-05 19:07:15 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,27 +1,27 @@
-class Author 
+class Author
 
-  attr_accessor :name, :posts 
-  @@posts = [] 
+  attr_accessor :name, :posts
+  @@posts = []
 
-  def initialize(name) 
-    @name = name 
-    @posts = [] 
-  end 
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
 
-  def add_post(post) 
-    @@posts << post 
-    @posts << post 
-    post.author = self 
-  end 
+  def add_post(post)
+    @@posts << post
+    @posts << post
+    post.author = self
+  end
 
-  def add_post_by_title(post_title) 
-    post = Post.new(post_title) 
-    @@posts << post 
-    @posts << post 
-    post.author = self 
-  end 
+  def add_post_by_title(post_title)
+    post = Post.new(post_title)
+    @@posts << post
+    @posts << post
+    post.author = self
+  end
 
-  def self.post_count 
-    @@posts.count 
-  end 
+  def self.post_count
+    @@posts.count
+  end
 end 
